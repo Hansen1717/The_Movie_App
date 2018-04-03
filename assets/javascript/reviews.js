c5635a44e12d525bb2352b9c9a0374a958a9fa8c
Bryan Hansen <bhansen1717@gmail.com>
2018-04-03 19:15:09 -0400

    reviews page

---
+++ assets/javascript/reviews.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    var movie = "The Lion King";
+    var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+
+    // Creating an AJAX call for the specific movie button being clicked
+    var showReviewAndLinks = function () {
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            for (var i = 0; i < response.Ratings.length; i++) {
+                var panelHeading = '#review-heading-' + i;
+                var panelBody = '#review-body-' + i;
+                var heading = response.Ratings[i].Source;
+                var review = heading + " scores " + "<span class='title-span'>" + movie + "</span>" + " at " + response.Ratings[i].Value;
+                $(panelHeading).html(heading);
+                $(panelBody).html(review);
+            }
+        });
+    };
+})
